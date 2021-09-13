e7a88ee5b96852e75f3cc44c2e27c850057f39e0
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2021-09-13 22:23:09 +0300

    fix: incorrect padding

--- services/position_py/main.py
+++ services/position_py/main.py
@@ -13,23 +13,24 @@ if __name__ == "__main__":
     with NATSClient() as client:
         client.connect()
 
-    try:
-        while True:
-            frames = pipe.wait_for_frames()
-            pose = frames.get_pose_frame()
+        try:
+            while True:
+                frames = pipe.wait_for_frames()
+                pose = frames.get_pose_frame()
 
-            if pose:
-                data = pose.get_pose_data()
+                if pose:
+                    data = pose.get_pose_data()
 
-                frame = position_pb2.Position()
+                    frame = position_pb2.Position()
 
-                position = vector3_pb2.Vector3()
-                position.x = data.translation.x
-                position.y = data.translation.y
-                position.z = data.translation.z
+                    position = vector3_pb2.Vector3()
+                    position.x = data.translation.x
+                    position.y = data.translation.y
+                    position.z = data.translation.z
 
-                frame.position.CopyFrom(position)
+                    frame.position.CopyFrom(position)
 
-                client.publish("position", payload=frame.SerializeToString())
-    finally:
-        pipe.stop()
+                    client.publish(
+                        "position", payload=frame.SerializeToString())
+        finally:
+            pipe.stop()
