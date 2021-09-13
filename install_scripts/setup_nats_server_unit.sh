361e9d2117ec1df86a3edbc803a7f80b2cc11f10
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2021-09-13 22:49:05 +0300

    feat: script for setting up nats-server unit

---
+++ install_scripts/setup_nats_server_unit.sh
@@ -0,0 +1,14 @@
+cat >/etc/systemd/system/nats.service <<EOF
+[Unit]
+Description=NATS server
+
+[Service]
+ExecStart=/usr/bin/nats-server
+User=$(id -un)
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable nats
+systemctl start nats
