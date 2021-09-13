3159d0de866d13ea6a8974143a0a294608b2213e
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2021-09-13 14:22:53 +0300

    feat: installation scripts

---
+++ install_scripts/build_realsense.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/JetsonHacksNano/installLibrealsense
+cd installLibrealsense
+./buildLibrealsense.sh
---
+++ install_scripts/resize_swap.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/JetsonHacksNano/resizeSwapMemory
+cd resizeSwapMemory
+./setSwapMemorySize.sh -g 4
+reboot
