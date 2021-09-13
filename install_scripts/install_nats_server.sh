7ae480d5759af220caf72420366bad4fbedb22ba
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2021-09-13 15:32:50 +0300

    fix: bash syntax

--- install_scripts/install_nats_server.sh
+++ install_scripts/install_nats_server.sh
@@ -1,4 +1,4 @@
-$tag = curl -s https://github.com/nats-io/nats-server/releases/latest | grep -o "v[0-9.]\+"
+tag=$(curl -s https://github.com/nats-io/nats-server/releases/latest | grep -o "v[0-9.]\+")
 curl -L https://github.com/nats-io/nats-server/releases/download/$tag/nats-server-$tag-linux-amd64.zip -o nats-server.zip
 unzip nats-server.zip -d nats-server
 cp nats-server/nats-server-$tag-linux-amd64/nats-server /usr/bin
