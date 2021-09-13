eb685f6533719000c0db918eba813ca15caf29a4
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2021-09-13 23:17:28 +0300

    feat: script for building unit files for services

---
+++ build_scripts/build_unit_services.sh
@@ -0,0 +1,36 @@
+services_py=$(find services/**_py/main.py)
+services_rs=$(find services/**_rs/Cargo.toml)
+for service in $services_py; do
+    service_name=$(basename $(dirname $service))
+    cat >services/$service_name/$service_name.service <<EOF
+[Unit]
+Description=NATS $service_name service
+After=nats.service
+
+[Service]
+Environment=PYTHONPATH=:/usr/local/lib/python3.6/pyrealsense2
+ExecStart=/usr/bin/python3 $(realpath $service)
+User=$(id -un)
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+done;
+for service in $services_rs; do
+    service_name=$(basename $(dirname $service))
+    service_bin=$(realpath services/$service_name/target/release/$service_name)
+    cat >services/$service_name/$service_name.service <<EOF
+[Unit]
+Description=NATS $service_name service
+After=nats.service
+
+[Service]
+ExecStart=$service_bin
+User=$(id -un)
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+done;
