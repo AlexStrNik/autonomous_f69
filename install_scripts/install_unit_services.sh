72b474553150712a6c8f285447d6dad8aee20576
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2021-09-13 23:36:59 +0300

    fix: units installation script

--- install_scripts/install_unit_services.sh
+++ install_scripts/install_unit_services.sh
@@ -1,7 +1,7 @@
 units=$(find services/**/*.service)
 for unit in $units; do
-    unit_name=$(filename $unit);
-    cp unit /etc/systemd/system/$unit_name;
+    unit_name=$(basename $unit);
+    cp $unit /etc/systemd/system/$unit_name;
     systemctl enable $unit_name;
     systemctl start $unit_name;
 done;
