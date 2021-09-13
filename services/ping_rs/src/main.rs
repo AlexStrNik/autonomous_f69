db0b3bbe781792c21eb08bc8efb17bde27cefac2
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2021-09-13 18:54:00 +0300

    feat: rust service stub

---
+++ services/ping_rs/src/main.rs
@@ -0,0 +1,20 @@
+mod _schemas;
+
+use protobuf::Message;
+use _schemas::ping::*;
+
+fn main() {
+    let client = nats::connect("nats://127.0.0.1:4222").unwrap();
+
+    let sub = client.subscribe("ping").unwrap();
+
+    for msg in sub.messages() {
+        let ping_request = PingRequest::parse_from_bytes(&msg.data).unwrap();
+
+        let mut pong_response = PongResponse::new();
+        pong_response.number = ping_request.number + 1;
+        let bytes = pong_response.write_to_bytes().unwrap();
+
+        msg.respond(bytes).unwrap();
+    }
+}
