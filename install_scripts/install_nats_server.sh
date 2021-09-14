580d1d02c4715bd14faf4822222129a0bd88247d
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2021-09-14 17:23:29 +0300

    chore: remove artifacts after installing `nats-server`

--- install_scripts/install_nats_server.sh
+++ install_scripts/install_nats_server.sh
@@ -2,3 +2,5 @@ tag=$(curl -s https://github.com/nats-io/nats-server/releases/latest | grep -o "
 curl -L https://github.com/nats-io/nats-server/releases/download/$tag/nats-server-$tag-linux-arm64.zip -o nats-server.zip
 unzip nats-server.zip -d nats-server
 cp nats-server/nats-server-$tag-linux-arm64/nats-server /usr/bin
+rm -rf nats-server
+rm -rf nats-server.zip
