587fcc61c9d1582fb480dfd1f4a4e0eca05c5bee
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2021-09-14 13:11:19 +0300

    feat: websocket config for NATS server

--- install_scripts/setup_nats_server_unit.sh
+++ install_scripts/setup_nats_server_unit.sh
@@ -1,10 +1,16 @@
+cat >/etc/nats.conf <<EOF
+websocket: {
+  port: 4223
+  no_tls: true
+}
+EOF
 cat >/etc/systemd/system/nats.service <<EOF
 [Unit]
 Description=NATS server
 After=network.target
 
 [Service]
-ExecStart=/usr/bin/nats-server
+ExecStart=/usr/bin/nats-server -c /etc/nats.conf
 User=$(id -un)
 Restart=on-failure
 
