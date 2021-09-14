51cc42d377cdabd36119b0f323b3333d45021f82
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2021-09-14 14:02:20 +0300

    feat: add script for building viewer

---
+++ build_scripts/build_viewer.sh
@@ -0,0 +1,9 @@
+cd tools/viewer
+npm run build
+build=$(realpath build)
+cat >build/start.sh <<EOF
+cd $build
+{ sleep 1; python -m webbrowser http://localhost:8000; } &
+python3 -m http.server
+EOF
+chmod +x build/start.sh
